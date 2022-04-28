3db38bd0d292f6149654e7e1c1a0ad92c8d086e7
rokill3r <rokill3r@gmail.com>
2022-04-29 01:21:34 +0300

    Implemented compute() function to calculate and display corectly the interest results Implemented updateRate() function to automatically display the selected slider rate

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+// compute() function will calculate and display the total interest amount based on the intpu
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principalElement = document.getElementById("principal");
+    var principal = principalElement.value;
+    // If value is negative or empty the display error message
+    if ( principal == "" || principal <= 0 ){
+        alert("Enter a positive number")
+        principalElement.focus();
+        return false;
+    }
+    // Read and calculate values
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    // Display the results
+    document.getElementById("result")
+            .innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of \<mark\>"+amount+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>"
     
+    return true;
+}
+
+// UpdateRate() function will automatically display the seletected slider rate
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
